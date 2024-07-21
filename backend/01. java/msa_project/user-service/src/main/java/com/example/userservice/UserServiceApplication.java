f5bba8b625390322b689a737556fbb2f7b717884
우효식 <woohs0930@gmail.com>
2024-07-21 16:58:08 +0900

    2024-07-21 hswoo
    MSA 공부중(USER Account 계정정보 CRUD)

--- backend/01. java/msa_project/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ backend/01. java/msa_project/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,6 +3,8 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +14,13 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+    /**
+     * Spring 4 이후 버전은 annotaion으로 bean을 등록
+     * spring 3 버전 xml
+     *
+     * */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder()
+    }
 }
---
+++ backend/01. java/msa_project/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,32 @@
+package com.example.userservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+    private static final String[] WHITE_LIST = {
+            "/users/**",
+            "/**"
+    };
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(WHITE_LIST).permitAll());
+
+        return http.build();
+    }
+}
