cbd9cf10b7a49d0c3e06c76f8dfe80f3df560a50
woohs0930 <woohs0930@gmail.com>
2024-10-23 10:16:48 +0900

    2024-10-23 hswoo
    코루틴 작업중

---
+++ backend/02. kotlin/coroutinlector/coroutinlector/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "info.coroutin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // 코루틴 라이브러리
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
