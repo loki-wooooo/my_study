18b67a010ba8fc2c550c57f4285f2d541542418f
우효식 <woohs0930@gmail.com>
2024-11-12 22:25:57 +0900

    2024-11-12 hswoo
    kotlin 변경처리

---
+++ backend/02. kotlin/spring-boot/library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -0,0 +1,30 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserService(
+    private val userRepository: UserRepository
+) {
+
+    @Transactional
+    fun saveUser(requesat: UserCreateRequest) {
+        val newUser = User(null, requesat.name, requesat.age)
+        userRepository.save(newUser)
+    }
+
+    @Transactional(readOnly = true)
+    fun getUsers(): List<UserResponse> {
+
+        //존재하는 유저를 UserResponse로 변경처리
+        return userRepository.findAll().map { user ->
+            UserResponse(user)
+        }
+
+    }
+}
---
+++ backend/02. kotlin/spring-boot/library-app/src/main/kotlin/com/group/libraryapp/utils/ExceptionUtils.kt
@@ -0,0 +1,5 @@
+package com.group.libraryapp.utils
+
+fun fail(): Nothing {
+    throw IllegalArgumentException()
+}
