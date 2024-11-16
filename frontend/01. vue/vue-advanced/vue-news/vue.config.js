a7166b00828abee18b6ad5512617c7f801fc6fd1
hswoo <woohs0930@gmail.com>
2024-11-16 19:48:06 +0900

    2024-11-16 hswoo
    eslint 끄기

--- frontend/01. vue/vue-advanced/vue-news/vue.config.js
+++ frontend/01. vue/vue-advanced/vue-news/vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  //eslint 설정 끄기
+  lintOnSave: false,
+};
