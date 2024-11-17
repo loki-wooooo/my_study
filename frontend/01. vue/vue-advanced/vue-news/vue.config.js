fec457938b7eee001d29bb139b9f6b6206947bc0
hswoo <woohs0930@gmail.com>
2024-11-17 18:29:22 +0900

    Auto stash before merge of "main" and "origin/main"

--- frontend/01. vue/vue-advanced/vue-news/vue.config.js
+++
@@ -1,9 +0,0 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-
-module.exports = {
-  //eslint 설정 끄기
-  lintOnSave: false,
-};
