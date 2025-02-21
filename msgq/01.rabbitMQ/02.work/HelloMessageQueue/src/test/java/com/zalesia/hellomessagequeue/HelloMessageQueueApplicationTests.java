8268e1c7e86264f5ee63df1955ac6040aa3b3c20
woohs0930 <woohs0930@gmail.com>
2025-02-21 16:50:45 +0900

    2025-02-21 hswoo
    rabbitmq 먼저 진행 중

---
+++ msgq/01.rabbitMQ/02.work/HelloMessageQueue/src/test/java/com/zalesia/hellomessagequeue/HelloMessageQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zalesia.hellomessagequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessageQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
