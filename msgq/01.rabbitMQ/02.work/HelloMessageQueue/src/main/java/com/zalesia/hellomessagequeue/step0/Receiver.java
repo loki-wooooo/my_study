a25e9d1df07930503cf83f5bf266f6a6f4b65c3b
woohs <woohs0930@gmail.com>
2025-02-22 14:57:23 +0900

    2025-02-22 hswoo
    rabbitmq config 설정 및 producer/consumer 간단 설정

---
+++ msgq/01.rabbitMQ/02.work/HelloMessageQueue/src/main/java/com/zalesia/hellomessagequeue/step0/Receiver.java
@@ -0,0 +1,15 @@
+package com.zalesia.hellomessagequeue.step0;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 실제 컨슈머 역할
+ * */
+@Component
+public class Receiver {
+
+    public void receiveMessage(String message) {
+        System.out.println("[#] Received: " + message);
+    }
+
+}
