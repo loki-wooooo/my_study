9b9a2823ac33b98d2b4d2bea647175592c11ec33
우효식 <woohs0930@gmail.com>
2025-04-05 18:55:49 +0900

    2025-04-05 hswoo
    * algorithm
    - lucky wheel 진행중

---
+++ algorithm/18.lucky_wheel/lucky_wheel/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * 문제: 바퀴를 시계방향으로 S칸 돌릴때마다 화살표가 가리키는 글자가 주어질 떄 바퀴의 각 칸에 적어놓은 알파벳을 알아내자
+         * 
+         *
+         * */
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+    }
+}
