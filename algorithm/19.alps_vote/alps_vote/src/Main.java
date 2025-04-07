4818e94656aafe0d287e1776769a5e38cd180d43
hswoo <woohs0930@gmail.com>
2025-04-07 23:28:14 +0900

    2025-04-07 hswoo
    *algorithm
    - alps 투표 진행 중
    - https://www.acmicpc.net/problem/2817

---
+++ algorithm/19.alps_vote/alps_vote/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * 문제: 득표수가 주어질 떄 (중략)에 따라 스태프가 받을 칩의 개수를 구해보자.
+         * 1. 전체 득표수의 5% 미만의 스태프를 후보에서 제외
+         * 2. 남은 스태프마다 받은 득표수를 1~14로 나눈 점수를 얻는다.
+         * 3. 전체 점수 집합에서 점수가 큰 1~14번 스태프에게 칩을 1개씩 지급
+         * 4. 스태프 이름에 대해 사전순으로 후보 스태프와 받은 칩의 수를 출력
+         *
+         * */
+    }
+}
