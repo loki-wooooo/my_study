fe0643f34d86a1ae85c71e27491d109d0f982e3a
woohs <woohs0930@gmail.com>
2025-09-07 11:43:23 +0900

    2025-09-07 hswoo
    router transition 추가

--- frontend/01. vue/vue-advanced/vue-news/App.vue
+++ frontend/01. vue/vue-advanced/vue-news/App.vue
@@ -3,7 +3,12 @@
   <!-- <Toolbar></Toolbar> -->
   <!--2. Essential-->
   <tool-bar></tool-bar>
-  <router-view></router-view>
+
+  <!-- route transition -->
+  <transition name="page">
+    <router-view></router-view>
+  </transition>
+
 </template>
 
 <script>
@@ -38,5 +43,14 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+
+/*ROUTE TRANSITION*/
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+
+.page-enter, .page-leave-to {
+  opacity: 0;
 }
 </style>
