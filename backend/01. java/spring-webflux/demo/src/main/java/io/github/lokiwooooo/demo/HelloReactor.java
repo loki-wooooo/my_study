e008a06f02a2995e067d338ea3978279680fb8b9
hswoo <woohs0930@gmail.com>
2025-12-01 08:07:23 +0900

    2025-12-01 hswoo
    reactor 내용 추가

--- backend/01. java/spring-webflux/demo/src/main/java/io/github/lokiwooooo/demo/HelloReactor.java
+++ backend/01. java/spring-webflux/demo/src/main/java/io/github/lokiwooooo/demo/HelloReactor.java
@@ -1,10 +1,21 @@
 package io.github.lokiwooooo.demo;
 
-import reactor.core.publisher.Mono;
+import io.github.lokiwooooo.demo.util.LogType;
+import io.github.lokiwooooo.demo.util.Logger;
+import reactor.core.publisher.Flux;
+
 
 public class HelloReactor {
     public static void main(String[] args) {
-        Mono.just("Hello Reactor") // publisher mono
-                .subscribe(message -> System.out.println(message));
+//        Mono.just("Hello Reactor") // publisher mono
+//                .subscribe(message -> System.out.println(message));
+
+        // Flux -> publisher
+
+        Flux<String> sequence = Flux.just("Hello", "Reactor");
+        sequence
+                .map(data -> data.toLowerCase())
+                .subscribe(data -> Logger.log(LogType.ON_NEXT, data));
+
     }
 }
