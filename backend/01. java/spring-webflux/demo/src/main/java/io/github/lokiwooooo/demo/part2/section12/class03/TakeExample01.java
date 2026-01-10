811679b70625404655077530dabf590a9d5740d9
woohs0930 <woohs0930@gmail.com>
2026-01-10 17:24:35 +0900

    2026-01-10 hswoo Reactor Take 기본 예제

---
+++ backend/01. java/spring-webflux/demo/src/main/java/io/github/lokiwooooo/demo/part2/section12/class03/TakeExample01.java
@@ -0,0 +1,25 @@
+package io.github.lokiwooooo.demo.part2.section12.class03;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+/**
+ * take 기본 예제
+ * - 파라미터로 입력한 숫자만큼만 DownStream으로 emit 한다.
+ *
+ *
+ */
+@Slf4j
+public class TakeExample01 {
+    public static void main(String[] args) throws InterruptedException {
+        Flux
+                .interval(Duration.ofSeconds(1))
+                .doOnNext(data -> log.info("data: {}", data))
+                .take(3)
+                .subscribe(data -> log.info("subscrbe: {}", data));
+
+        Thread.sleep(5000L);
+    }
+}
